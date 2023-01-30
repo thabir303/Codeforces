44f239a67fb517589edc0c54ee95aed7e9b39303
thabir303 <109710837+thabir303@users.noreply.github.com>
2023-01-30 18:37:16 +0600

    Add files via upload

---
+++ sample.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define endl "\n"
+#define yes cout<<"YES"<<endl;
+#define no cout<<"NO"<<endl;
+
+int main()
+{
+   ll t; cin>>t;
+   while(t--)
+   {
+
+
+   }
+
+}
